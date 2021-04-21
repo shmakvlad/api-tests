9300021bfd86a17e122603c89185dcc05ed83674
Shmakov Vlad <shmakvlad@gmail.com>
2021-04-21 16:52:04 +0300

    add one more example

--- api/src/test/java/com/affise/tests/Run/Awaitility.java
+++ api/src/test/java/com/affise/tests/Run/Awaitility.java
@@ -95,4 +95,92 @@ public class Awaitility {
         }
         throw new RuntimeException("Failed to get a valid click");
     }*/
+
+
+// 6
+
+    /*public static ConversionResponse generateConversionByClick(ClickResponse click) {
+        return validateConversion(() -> generateConversion(click));
+    }
+
+    public static ConversionResponse generateConversionWithStatus(ClickResponse click, int status) {
+        return validateConversion(() -> generateConversion(click, status));
+    }
+
+    public static ConversionResponse generateConversionWithGoal(ClickResponse click, String goal) {
+        return validateConversion(() -> generateConversion(click, goal));
+    }
+
+    public static ConversionResponse validateConversion(Supplier<ConversionResponse> supplier) {
+        final ConversionResponse[] conversion = new ConversionResponse[]{new ConversionResponse()};
+        conversionConfigurationTimeout().until(() -> {
+                    conversion[0] = supplier.get();
+                    return getConversionStatus(conversion[0]);
+                }, IsEqual.equalTo(1)
+        );
+        return conversion[0];
+    }
+
+    public static ConditionFactory conversionConfigurationTimeout() {
+        return await().ignoreExceptions()
+                .atMost(Duration.ofMillis(getConfig().conversionTimeout()))
+                .pollDelay(Duration.ofMillis(getConfig().conversionRequestDelayTimeout()))
+                .pollInterval(Duration.ofMillis(getConfig().conversionRequestIntervalTimeout()));
+    }
+
+    public static ConditionFactory waitForConversionCreateConfig() {
+        return await().ignoreExceptions()
+                .atMost(Duration.ofSeconds(20))
+                .pollDelay(Duration.ofSeconds(15))
+                .pollInterval(Duration.ofSeconds(4));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 'confirmed', 31",
+            "2, 'pending',   41",
+            "3, 'declined',  51",
+            "5, 'hold',      61"
+    })
+    @DisplayName("Conversions come with expected status")
+    void getConversionsStatus1(int intStatus, String stringStatus, String limit) {
+
+        ClickResponse click = makeClick();
+        generateConversionWithStatus(click, intStatus);
+        String conversionId = getConversionIdByClickId(click.getClickid());
+
+        waitForConversionCreateConfig().alias("Wait for the conversion to be created").until(
+                () -> statisticsConversionApiService.checkStatisticsConversions(ROOT_ADMIN_KEY, dateFrom, dateTo, limit)
+                        .getResponse().path(String.format("conversions.find{it.id == '%s'}.id", conversionId)),
+                equalTo(conversionId));
+
+        ConversionsList conversionsList =
+                statisticsConversionApiService.checkStatisticsConversions(ROOT_ADMIN_KEY, dateFrom, dateTo, limit)
+                        .asClass(ConversionsList.class);
+
+        assertThat(conversionsList.getConversions().stream()
+                .filter(x -> x.getId().equals(conversionId))
+                .map(Conversion::getStatus).findFirst().get())
+                .isEqualTo(stringStatus);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 'confirmed', 35",
+            "2, 'pending',   36",
+            "3, 'declined',  37",
+            "5, 'hold',      38"
+    })
+    @DisplayName("Conversions come with expected status")
+    void getConversionsStatus2(int intStatus, String stringStatus, String limit) {
+
+        ClickResponse click = makeClick();
+        generateConversionWithStatus(click, intStatus);
+
+        waitForConversionCreateConfig().alias("Wait for the conversion to be created").until(() ->
+                        statisticsConversionApiService.checkStatisticsConversions(ROOT_ADMIN_KEY, dateFrom, dateTo, limit)
+                                .getResponse().path(String.format("conversions.find{it.action_id == '%s'}.status", click.getClickid())),
+                equalTo(stringStatus));
+    }*/
+
 }
